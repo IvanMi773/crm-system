2062dc743b2b687630c69c26c2dfa4ec5e927088
IvanMi773 <imihajlovskij23@gmail.com>
2020-04-23 14:28:15 +0300

    create views

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,56 @@
+<template>
+	<ul class="sidenav app-sidenav" :class="{open: value}">
+		<router-link
+			tag="li"
+			active-class="active"
+			v-for="link in links"
+			:to="link.url"
+			:key="link.url"
+			:exact="link.exect"
+		>
+			<a href="#" class="waves-effect waves-orange pointer">
+                {{ link.title }}
+            </a>
+		</router-link>
+	</ul>
+</template>
+
+<script>
+	export default {
+        props: {
+            value: Boolean,
+        },
+
+		data() {
+			return {
+				links: [
+					{
+						title: 'Счет',
+						url: '/',
+						exact: true,
+					},
+
+					{
+						title: 'История',
+						url: '/history',
+					},
+
+					{
+						title: 'Планирование',
+						url: '/planning',
+					},
+
+					{
+						title: 'Новая запись',
+						url: '/record',
+					},
+
+					{
+						title: 'Категории',
+						url: '/categories',
+					},
+				],
+			};
+		},
+	};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,77 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
+	{
+		path: '/',
+		name: 'Home',
+		meta: { layout: 'main' },
+		component: () => import('../views/Home.vue'),
+	},
+
+	{
+		path: '/login',
+		name: 'login',
+		meta: { layout: 'empty' },
+		component: () => import('../views/Login.vue'),
+	},
+
+	{
+		path: '/register',
+		name: 'register',
+		meta: { layout: 'empty' },
+		component: () => import('../views/Register.vue'),
+	},
+
+	{
+		path: '/categories',
+		name: 'categories',
+		component: () => import('../views/Categories.vue'),
+		meta: { layout: 'main' },
+	},
+
+	{
+		path: '/detail',
+		name: 'detail',
+		component: () => import('../views/Detail.vue'),
+		meta: { layout: 'main' },
+	},
+
+	{
+		path: '/history',
+		name: 'history',
+		component: () => import('../views/History.vue'),
+		meta: { layout: 'main' },
+	},
+
+	{
+		path: '/planning',
+		name: 'planning',
+		component: () => import('../views/Planning.vue'),
+		meta: { layout: 'main' },
+	},
+
+	{
+		path: '/profile',
+		name: 'profile',
+		component: () => import('../views/Profile.vue'),
+		meta: { layout: 'main' },
+	},
+
+	{
+		path: '/record',
+		name: 'record',
+		component: () => import('../views/Record.vue'),
+		meta: { layout: 'main' },
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
 });
 
 export default router;
